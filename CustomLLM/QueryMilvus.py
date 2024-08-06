143693722aea70ebbbc46ba521dc0e18a3feb67a
eric.zhou <19791685+ericzzhou@users.noreply.github.com>
2024-08-06 12:35:25 +0800

    connecton milvus

--- CustomLLM/QueryMilvus.py
+++ CustomLLM/QueryMilvus.py
@@ -3,8 +3,10 @@ from fastapi import FastAPI, HTTPException, Request, Response
 from fastapi.responses import JSONResponse, StreamingResponse
 from pydantic import BaseModel, Field
 import uuid
-from typing import Optional, List
+from typing import Generator, Optional, List
 import logging
+from pymilvus import connections, Collection, CollectionSchema, FieldSchema, DataType
+import requests
 
 # 日志
 logging.basicConfig(
@@ -14,6 +16,49 @@ logger = logging.getLogger(__name__)
 
 app = FastAPI()
 PORT = 8765
+
+
+connections.connect(alias="default", host="44.236.217.101", port="19530")
+
+
+def get_vector_embedding(keyword):
+    # 定义请求的 URL 和参数
+    url = f"https://bge.yamibuy.net/zh/bge/vector"
+    params = {"keyword": keyword}
+
+    try:
+        # 发送 GET 请求
+        response = requests.get(url, params=params)
+
+        # 检查响应状态码
+        response.raise_for_status()
+
+        # 返回 JSON 数据
+        return response.json()
+
+    except requests.RequestException as e:
+        # 处理请求异常
+        print(f"请求错误: {e}")
+        return None
+
+
+def convert_string_to_float_array(embedding_str):
+    """
+    将包含浮点数的字符串转换为浮点数数组
+
+    参数:
+    embedding_str (str): 包含浮点数的字符串，每个浮点数由逗号分隔
+
+    返回:
+    List[float]: 转换后的浮点数数组
+    """
+    # 将字符串分割成列表
+    embedding_list = embedding_str.split(",")
+
+    # 将列表中的字符串转换为浮点数
+    embedding_floats = [float(num) for num in embedding_list]
+
+    return embedding_floats
 
 
 # 数据模型
@@ -63,7 +108,7 @@ async def list_models():
     current_time = int(time.time())
     available_models = [
         {
-            "id": "milvus:yami_keyword_embedding",
+            "id": "milvus:yami_test_item_zh_embedding",
             "object": "model",
             "created": current_time - 100000,
             "owned_by": "yami@eric",
@@ -82,8 +127,46 @@ async def list_models():
 
 # 模拟的聊天功能
 def generate_response(prompt, model_id="mock-model-v1"):
-    # 这里可以是一个复杂的生成逻辑，比如基于模型的回答
-    return f"Response from {model_id}: Echo: {prompt}"
+    milvus_collection_name = model_id.split(":")[1].strip()
+
+    db = Collection(name=milvus_collection_name)
+    print("schema \n")
+    print(db.schema)
+    # 加载集合
+    db.load()
+
+    print(f"集合 {milvus_collection_name} 已加载")
+
+    query_vector = get_vector_embedding(prompt)["data"]
+    embedding_list = convert_string_to_float_array(query_vector)
+    print("完成keyword向量化")
+    search_param = {
+        "metric_type": "COSINE",
+        "index_type": "IVF_FLAT",
+        "params": {"nprobe": 10},
+    }
+
+    results = db.search(
+        data=[embedding_list],
+        anns_field="embedding",  # 向量字段名称
+        param=search_param,
+        limit=10,
+        expr=None,
+        output_fields=["item_number"],
+    )
+    print("milvus 查询完成")
+    items = []
+    for result in results:
+        for hit in result:
+            goods_number = hit.entity.get("item_number")
+            items.append(goods_number)
+            print(goods_number)
+
+    # 查询数据库
+
+    # # 这里可以是一个复杂的生成逻辑，比如基于模型的回答
+    # return f"Response from {model_id}: Echo: {prompt},Result:{results}"
+    return " \n ".join(items)  # 或者使用其他合适的连接符号
 
 
 def load_user_prompt(request: ChatCompletionRequest):
